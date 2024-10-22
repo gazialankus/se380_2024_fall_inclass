bb6a470aee930ca37e28260c0bc10f7ebdede858
Gazihan Alankus <gazialankus@gmail.com>
2024-10-22 13:38:20 +0300

    Is able to reflect initial count on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
 
+class Person {
+  Person(this.name);
+
+  final String name;
+}
+
+void myF(Person? p) {
+  Person px;
+
+  px = Person('a');
+
+  px.name;
+
+  final a = p?.name;
+
+  if (p != null) {
+    final n = p.name;
+  }
+}
+
 void main() {
+  Person? a;
+
+  myF(a);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo during class',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -37,16 +44,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({required String title}) : this.title = title;
 
   final String title;
 
@@ -59,59 +57,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pressed this many times',
-            ),
-            Text(
-              '$_counter is the current number',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // MessageArea(data: 'You have pressed this many times: $_counter'),
+            Classroom(5),
+            // Text(
+            //   '$_counter is the current number',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
           ],
         ),
       ),
@@ -119,7 +85,60 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.accessible_outlined),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
+  }
+}
+
+class MessageArea extends StatefulWidget {
+  const MessageArea({
+    required this.data,
+  });
+
+  final String data; // props
+
+  @override
+  State<MessageArea> createState() => _MessageAreaState();
+}
+
+class _MessageAreaState extends State<MessageArea> {
+  late Person p; // state
+
+  @override
+  void initState() {
+    super.initState();
+    p = Person(widget.data);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      p.name,
+    );
+  }
+}
+
+
+class Classroom extends StatefulWidget {
+  const Classroom(this.desiredStudentCount);
+
+  final int desiredStudentCount;
+
+  @override
+  State<Classroom> createState() => _ClassroomState();
+}
+
+class _ClassroomState extends State<Classroom> {
+  late int initialStudentCount;
+
+  @override
+  void initState() {
+    super.initState();
+    initialStudentCount = widget.desiredStudentCount;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('initial count: $initialStudentCount', style: TextStyle(fontSize: 34),);
   }
 }
